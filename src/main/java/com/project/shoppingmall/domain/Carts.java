74137c2690df7c0caebf581b1280e38598e139f2
OSSING <kos1090291@gmail.com>
2023-12-18 22:46:11 +0900

    에러 수정 및 어노테이션 수정

--- src/main/java/com/project/shoppingmall/domain/Carts.java
+++ src/main/java/com/project/shoppingmall/domain/Carts.java
@@ -3,30 +3,27 @@ package com.project.shoppingmall.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.math.BigInteger;
-
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "Carts")
 public class Carts {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long CartID;
 
     @ManyToOne
-    @JoinColumn(name = "userID")
     private Users userID;
 
-//    private items itemID;
+    @ManyToOne
+    private Items itemID;
 
     private int stock;
 
     private int price;
 
-    private boolean check;
+    private boolean checked;
 }
