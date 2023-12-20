a311110bd14a8309b8ab6c98897d746e61d979f6
A-nijy <rainick98@gmail.com>
2023-12-20 15:02:48 +0900

    (관리자) 주문 내역 목록, 상세 조회와 상태 수정

---
+++ src/main/java/com/project/shoppingmall/dto/admin/response/OrderResponse.java
@@ -0,0 +1,57 @@
+package com.project.shoppingmall.dto.admin.response;
+
+import com.project.shoppingmall.domain.Orders;
+import com.project.shoppingmall.domain.Users;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Getter @Setter
+@AllArgsConstructor
+public class OrderResponse {
+
+    private Long orderID;
+
+    private Users userID;
+
+    private LocalDateTime orderedDate;
+
+    private LocalDateTime sippedDate;
+
+    private int price;
+
+    private String name;
+
+    private String address;
+
+    private String detAddress;
+
+    private String request;
+
+    private String tel;
+
+    private String status;
+
+    private String refundText;
+
+
+    public OrderResponse(Orders order){
+        orderID = order.getOrderID();
+        userID = order.getUserID();
+        orderedDate = order.getOrderedDate();
+        sippedDate = order.getSippedDate();
+        price = order.getPrice();
+        name = order.getName();
+        address = order.getAddress();
+        detAddress = order.getDetAddress();
+        request = order.getRequest();
+        tel = order.getTel();
+        status = order.getStatus();
+        refundText = order.getRefundText();
+    }
+
+}
