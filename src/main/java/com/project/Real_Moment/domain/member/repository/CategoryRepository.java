29f15ce0b12b50baf5521d24afce451abf99529b
OSSING <kos1090291@gmail.com>
2024-02-24 23:13:29 +0900

    카테고리 구현

--- src/main/java/com/project/Real_Moment/domain/member/repository/CategoryRepository.java
+++ src/main/java/com/project/Real_Moment/domain/member/repository/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.project.Real_Moment.domain.member.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 }
