628b48b61f2ecc039e1b0ea0f700fb9674be2806
OSSING <kos1090291@gmail.com>
2024-03-01 11:38:48 +0900

    conflict 수정

--- src/main/java/com/project/Real_Moment/domain/member/entity/Address.java
+++ src/main/java/com/project/Real_Moment/domain/member/entity/Address.java
@@ -8,7 +8,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Addresses {
+public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/project/Real_Moment/domain/member/repository/AddressRepository.java
+++ src/main/java/com/project/Real_Moment/domain/member/repository/AddressRepository.java
@@ -1,12 +1,12 @@
 package com.project.Real_Moment.domain.member.repository;
 
-import com.project.Real_Moment.domain.member.entity.Addresses;
+import com.project.Real_Moment.domain.member.entity.Address;
 import com.project.Real_Moment.domain.member.repository.custom.AddressRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AddressesRepository extends JpaRepository<Addresses, Long>, AddressRepositoryCustom {
+public interface AddressRepository extends JpaRepository<Address, Long>, AddressRepositoryCustom {
 
-    List<Addresses> findAddressesByMemberId_MemberId(Long memberId);
+    List<Address> findAddressesByMemberId_MemberId(Long memberId);
 }
