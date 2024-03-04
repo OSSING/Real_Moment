07f7e7400eb2348e15ba2491c15e5b4669a27684
OSSING <kos1090291@gmail.com>
2024-03-04 17:33:15 +0900

    장바구니 추가 및 수정 API 구현

--- src/main/java/com/project/Real_Moment/domain/member/repository/WishRepository.java
+++ src/main/java/com/project/Real_Moment/domain/member/repository/WishRepository.java
@@ -1,11 +1,9 @@
 package com.project.Real_Moment.domain.member.repository;
 
-import com.project.Real_Moment.domain.member.entity.Cart;
 import com.project.Real_Moment.domain.member.entity.Item;
 import com.project.Real_Moment.domain.member.entity.Member;
 import com.project.Real_Moment.domain.member.entity.Wish;
 import com.project.Real_Moment.domain.member.repository.custom.WishRepositoryCustom;
-import com.project.Real_Moment.presentation.dto.WishDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
