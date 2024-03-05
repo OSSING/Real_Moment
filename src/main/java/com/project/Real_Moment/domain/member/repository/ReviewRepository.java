21438697ac61eafe982482ce6b454f89bff6c147
OSSING <kos1090291@gmail.com>
2024-03-05 12:09:56 +0900

    상품 상세 리뷰 목록 조회 API 구현

--- src/main/java/com/project/Real_Moment/domain/member/repository/ReviewRepository.java
+++ src/main/java/com/project/Real_Moment/domain/member/repository/ReviewRepository.java
@@ -1,7 +1,8 @@
 package com.project.Real_Moment.domain.member.repository;
 
 import com.project.Real_Moment.domain.member.entity.Review;
+import com.project.Real_Moment.domain.member.repository.custom.ReviewRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewsRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
 }
