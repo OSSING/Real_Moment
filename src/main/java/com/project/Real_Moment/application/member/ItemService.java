6eb1192afbc48eb07463dfdb82e04faf5a3b4852
OSSING <kos1090291@gmail.com>
2024-03-06 14:07:44 +0900

    상품 목록 조건부 필터 조회 API 리팩터링

--- src/main/java/com/project/Real_Moment/application/member/ItemService.java
+++ src/main/java/com/project/Real_Moment/application/member/ItemService.java
@@ -2,11 +2,17 @@ package com.project.Real_Moment.application.member;
 
 import com.project.Real_Moment.domain.member.entity.Item;
 import com.project.Real_Moment.domain.member.repository.ItemRepository;
+import com.project.Real_Moment.presentation.dto.CondDto;
 import com.project.Real_Moment.presentation.dto.ItemDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -16,8 +22,16 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
     @Transactional(readOnly = true)
-    public ItemDto.ItemCondResponse getItemList(String itemSort, Long categoryId, String itemName, Boolean isDelete, int nowPage) {
-        return itemRepository.findItemListByCond(itemSort, categoryId, itemName, isDelete, nowPage);
+    public ItemDto.ItemCondResponse getItemList(CondDto.ItemListCond dto) {
+        Pageable pageable = PageRequest.of(dto.getNowPage() - 1, 10);
+
+        Page<Item> itemList = itemRepository.findItemListByCond(pageable, dto);
+
+        List<ItemDto.ItemResponse> itemListDto = itemList.stream()
+                .map(ItemDto.ItemResponse::new)
+                .toList();
+
+        return new ItemDto.ItemCondResponse(itemListDto, itemList.getTotalPages(), dto.getNowPage());
     }
 
     @Transactional(readOnly = true)
