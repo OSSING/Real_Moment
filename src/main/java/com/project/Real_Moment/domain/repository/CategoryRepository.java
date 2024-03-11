bb3ccf6c5ce564f8d02a682e9d92897b034adf3f
OSSING <kos1090291@gmail.com>
2024-03-11 15:45:15 +0900

    관리자 카테고리 수정 API 구현

--- src/main/java/com/project/Real_Moment/domain/repository/CategoryRepository.java
+++ src/main/java/com/project/Real_Moment/domain/repository/CategoryRepository.java
@@ -1,8 +1,12 @@
 package com.project.Real_Moment.domain.repository;
 
 import com.project.Real_Moment.domain.entity.Category;
+import com.project.Real_Moment.domain.repository.custom.CategoryRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+import java.util.Optional;
 
+public interface CategoryRepository extends JpaRepository<Category, Long>, CategoryRepositoryCustom {
+
+    Optional<Category> findByParent_Id(Long parentId);
 }
