a54713c19164a0b686202221439005cb8a11a3e5
OSSING <kos1090291@gmail.com>
2024-03-19 10:27:59 +0900

    관리자 회원 등급 목록 조회 API 구현

---
+++ src/main/java/com/project/Real_Moment/application/admin/AdminGradeService.java
@@ -0,0 +1,26 @@
+package com.project.Real_Moment.application.admin;
+
+import com.project.Real_Moment.domain.repository.GradeRepository;
+import com.project.Real_Moment.presentation.dto.GradeDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AdminGradeService {
+    
+    private final GradeRepository gradeRepository;
+    
+    @Transactional(readOnly = true)
+    public List<GradeDto.GradeResponse> getGradeList() {
+        return gradeRepository.findAll()
+                .stream()
+                .map(GradeDto.GradeResponse::new)
+                .toList();
+    }
+}
